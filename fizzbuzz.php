6bf1f74d8c12c95b974a78670df84a2d4b172ec5
wataru.otsuji <wataru.otsuji@gmail.com>
2023-10-17 10:49:57 +0900

    update

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -20,25 +20,27 @@ echo $output_text;
 class FizzBuzz
 {
 
-    public function __construct()
-    {
-    }
-
     public function fizzBuzzExec($target_array, $skip_text, $limit)
     {
         $output_text = "";
         for ($i = 1; $i <= $limit; $i++) {
-            $output_line = "";
-            foreach ($target_array as $vals) {
-                if ($i % $vals[0] == 0) {
-                    $output_line .= $vals[1];
-                }
-            }
-            if( $output_line == "" ) {
-                $output_line = $skip_text;
-            }
+            $output_line = $this->fizzBuzzOutputLine($target_array, $skip_text, $i);
             $output_text .= $i . ":" . $output_line . "\n";
         }
         return $output_text;
+    }
+
+    private function fizzBuzzOutputLine($target_array, $skip_text, $check_int)
+    {
+        $output_line = "";
+        foreach ($target_array as $vals) {
+            if ($check_int % $vals[0] == 0) {
+                $output_line .= $vals[1];
+            }
+        }
+        if ($output_line == "") {
+            $output_line = $skip_text;
+        }
+        return $output_line;
     }
 }
